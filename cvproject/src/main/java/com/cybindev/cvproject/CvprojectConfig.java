a0f66f2104ee9de11b9939445b394a50114b591d
Nikolay Shelevoy <runtycybin@gmail.com>
2026-01-08 11:10:55 +0100

    ultimos cambios del micro

--- cvproject/src/main/java/com/cybindev/cvproject/CvprojectConfig.java
+++ cvproject/src/main/java/com/cybindev/cvproject/CvprojectConfig.java
@@ -1,7 +1,5 @@
 package com.cybindev.cvproject;
 
-import org.mapstruct.factory.Mappers;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
