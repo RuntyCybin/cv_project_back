eb5a1c1b656c94cd5ed5e6bc9b71769782da8c05
Nikolay Shelevoy <runtycybin@gmail.com>
2026-02-07 00:16:24 +0100

    refactor de la clase CursoResponseMapper

--- cursos/src/main/java/com/cybindev/cursos/domain/CursosResponseMapper.java
+++ cursos/src/main/java/com/cybindev/cursos/domain/CursosResponseMapper.java
@@ -1,6 +1,7 @@
 package com.cybindev.cursos.domain;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 @Mapper
@@ -8,13 +9,13 @@ public interface CursosResponseMapper {
 
   @Mappings({
       // Map fields from entity to DTO
-      @org.mapstruct.Mapping(target = "id", source = "entity.id"),
-      @org.mapstruct.Mapping(target = "nombre", source = "entity.titulo"),
-      @org.mapstruct.Mapping(target = "portal", source = "entity.portal"),
-      @org.mapstruct.Mapping(target = "url", source = "entity.url"),
-      @org.mapstruct.Mapping(target = "autor", source = "entity.autor"),
-      @org.mapstruct.Mapping(target = "descripcion", source = "entity.descripcion"),
-      @org.mapstruct.Mapping(target = "periodo", source = "entity.periodo")
+      @Mapping(target = "id", source = "entity.id"),
+      @Mapping(target = "nombre", source = "entity.titulo"),
+      @Mapping(target = "portal", source = "entity.portal"),
+      @Mapping(target = "url", source = "entity.url"),
+      @Mapping(target = "autor", source = "entity.autor"),
+      @Mapping(target = "descripcion", source = "entity.descripcion"),
+      @Mapping(target = "periodo", source = "entity.periodo")
   })
   CursoResponseDTO toDto(Curso entity);
 }
