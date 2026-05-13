e09cf3f5b6530b17fbd93da949641c3147ba12d1
Nikolay Shelevoy <runtycybin@gmail.com>
2026-05-13 16:42:48 +0200

    feature/SDC-45: exp controller refactor and dtos refactor

--- experiencias/src/main/java/com/cybindev/experiencia/domain/ExperienciaResponseDTO.java
+++ experiencias/src/main/java/com/cybindev/experiencia/domain/ExperienciaResponseDTO.java
@@ -3,10 +3,10 @@ package com.cybindev.experiencia.domain;
 import java.io.Serializable;
 
 public record ExperienciaResponseDTO(
-        Long id,
-        String puesto,
-        String empresa,
-        String descripcion,
-        String fechaInicio,
-        String fechaFin) implements Serializable {
+    Long id,
+    String puesto,
+    String empresa,
+    String descripcion,
+    String fechaInicio,
+    String fechaFin) implements Serializable {
 }
